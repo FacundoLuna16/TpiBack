482e9bd6ba9ca44391c80f148589678aa0d3c50b
Facundo Luna <89422@sistemas.frc.utn.edu.ar>
2023-11-08 08:29:59 -0300

    gateway funcionando

--- Gateway/src/main/java/com/trabajoPractico/Gateway/infrastructure/configuration/GWConfig.java
+++ Gateway/src/main/java/com/trabajoPractico/Gateway/infrastructure/configuration/GWConfig.java
@@ -23,9 +23,9 @@ public class GWConfig {
                                         @Value("${apunte-api-gw-kempes.url-microservicio-estaciones}") String uriEstaciones) {
         return builder.routes()
                 // Ruteo al Microservicio de Alquileres
-                .route(p -> p.path("/api/alquileres").uri(uriAlquileres))
+                .route(p -> p.path("/api/v1/alquileres").uri(uriAlquileres))
                 // Ruteo al Microservicio de Estaciones
-                .route(p -> p.path("/api/estaciones").uri(uriEstaciones))
+                .route(p -> p.path("/api/v1/estaciones").uri(uriEstaciones))
                 .build();
 
     }
@@ -33,41 +33,36 @@ public class GWConfig {
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
         http.authorizeExchange(exchanges -> exchanges
 
-                        // Esta ruta puede ser accedida por cualquiera, sin autorización
-                        .pathMatchers("/api/alquileres/**")
-                        .hasRole("KEMPES_ADMIN")
 
-                        .pathMatchers("/api/estaciones/**")
-                        .hasRole("KEMPES_ORGANIZADOR")
 
                         // Cualquier otra petición...
                         .anyExchange()
-                        .authenticated()
+                        .permitAll()
 
                 ).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .csrf(csrf -> csrf.disable());
         return http.build();
     }
-
-    @Bean
-    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
-        var jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
-        var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-
-        // Se especifica el nombre del claim a analizar
-        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
-        // Se agrega este prefijo en la conversión por una convención de Spring
-        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
-
-        // Se asocia el conversor de Authorities al Bean que convierte el token JWT a un objeto Authorization
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
-                new ReactiveJwtGrantedAuthoritiesConverterAdapter(grantedAuthoritiesConverter));
-        // También se puede cambiar el claim que corresponde al nombre que luego se utilizará en el objeto
-        // Authorization
-        // jwtAuthenticationConverter.setPrincipalClaimName("user_name");
-
-        return jwtAuthenticationConverter;
-    }
+//
+//    @Bean
+//    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+//        var jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+//        var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+//
+//        // Se especifica el nombre del claim a analizar
+//        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+//        // Se agrega este prefijo en la conversión por una convención de Spring
+//        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+//
+//        // Se asocia el conversor de Authorities al Bean que convierte el token JWT a un objeto Authorization
+//        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
+//                new ReactiveJwtGrantedAuthoritiesConverterAdapter(grantedAuthoritiesConverter));
+//        // También se puede cambiar el claim que corresponde al nombre que luego se utilizará en el objeto
+//        // Authorization
+//        // jwtAuthenticationConverter.setPrincipalClaimName("user_name");
+//
+//        return jwtAuthenticationConverter;
+//    }
 
 
 
--- alquiler/src/main/java/com/trabajoPractico/alquiler/application/controller/AlquilerController.java
+++ alquiler/src/main/java/com/trabajoPractico/alquiler/application/controller/AlquilerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/alquiler")
+@RequestMapping("/api/v1/alquileres")
 public class AlquilerController {
     private final AlquilerService alquilerService;
 
