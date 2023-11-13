a909308b9e1dbd6e1ec404a1c6082237b7a4cdea
Facundo Luna <89422@sistemas.frc.utn.edu.ar>
2023-11-12 21:05:06 -0300

    agragamos dockerfile

--- alquiler/src/main/java/com/trabajoPractico/alquiler/infrastructure/adapters/EstacionServiceImpl.java
+++ alquiler/src/main/java/com/trabajoPractico/alquiler/infrastructure/adapters/EstacionServiceImpl.java
@@ -2,23 +2,35 @@ package com.trabajoPractico.alquiler.infrastructure.adapters;
 
 
 import com.trabajoPractico.alquiler.domain.exchangePort.EstacionService;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.val;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 
 @Service
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
-@RequiredArgsConstructor
 public class EstacionServiceImpl implements EstacionService {
 
     RestTemplate restTemplate;
 
+    final String uriEstacionesById;
+
+    final String uriEstacionesDistanciaEntreEstaciones;
+
+    public EstacionServiceImpl(RestTemplate restTemplate,
+                               @Value("${tpi.url-microservicio-estaciones-get-id}") String uriEstacionesById,
+                               @Value("${tpi.url-microservicio-estaciones-distancia-estaciones}") String uriEstacionesDistanciaEntreEstaciones) {
+        this.restTemplate = restTemplate;
+        this.uriEstacionesById = uriEstacionesById;
+        this.uriEstacionesDistanciaEntreEstaciones = uriEstacionesDistanciaEntreEstaciones;
+    }
+
     @Override
     public Boolean validateIdEstacion(int id) {
-        val response = restTemplate.getForEntity("http://localhost:8082/api/v1/estaciones/{id}", EstacionResponse.class, id);
+
+        val response = restTemplate.getForEntity(uriEstacionesById, EstacionResponse.class, id);
 
         //validar que el status code sea 200 y que el body no sea null
         if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
@@ -33,8 +45,7 @@ public class EstacionServiceImpl implements EstacionService {
         //http://localhost:8081/api/v1/estaciones/distanciaEntreEstaciones?idEstacion1={idEstacionOrigen}&idEstacion2={idEstacionDestino}
         try {
 
-            val response = restTemplate.getForEntity("http://localhost:8082/api/v1/estaciones" +
-                            "/distanciaEntreEstaciones?idEstacion1={idEstacionOrigen}&idEstacion2={idEstacionDestino}"
+            val response = restTemplate.getForEntity(uriEstacionesDistanciaEntreEstaciones
                     , Double.class, idEstacionOrigen,idEstacionDestino);
 
             if (response.getStatusCode().is2xxSuccessful()  && response.getBody() != null){
